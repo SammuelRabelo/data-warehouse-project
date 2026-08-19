392743eb56cdc512f734536b3000ecb01a1ed2b3
Sammuel Rabelo <sammuellara2012@gmail.com>
2026-08-18 21:03:55 -0300

    Fix quote style in init_database.sql comments
    Updated quotes in SQL script from backticks to single quotes for consistency.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -4,11 +4,11 @@ Criar banco de dados e schemas
 ================================
 
 Proposta do Script:
-	Este script cria um novo banco de dados com nome de `DataWareHouse` depois de checar se ele já existe.
-	Se o banco de dados já existir ele exclui e recria. O Script também cria 3 schemas dentro do banco de dados: `bronze`, `silver`, `gold`.
+	Este script cria um novo banco de dados com nome de 'DataWareHouse' depois de checar se ele já existe.
+	Se o banco de dados já existir ele exclui e recria. O Script também cria 3 schemas dentro do banco de dados: 'bronze', 'silver', 'gold'.
 
 Atenção:
-	Ao rodar esse script ele vai excluir o banco de dados `DataWareHouse` por completo se ele já existir.
+	Ao rodar esse script ele vai excluir o banco de dados 'DataWareHouse' por completo se ele já existir.
 	Todos os dados no banco serão permanentemente excluídos. Prossiga com atenção e tendo certeza que você backups antes de rodar o script.
 */
 
@@ -17,7 +17,7 @@ USE master;
 GO
 
 -- Excluir e recriar banco de dados `DataWareHouse`
-IF EXISTS (SELECT 1 FROM sys.database WHERE name = `DataWareHouse`)
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'DataWareHouse')
 BEGIN
 	ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWareHouse;
